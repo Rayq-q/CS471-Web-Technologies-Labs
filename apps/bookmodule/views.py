f34081973a0c159751bcc42fc38100c9bfbd08ee
Rqyq <mohammed.sa.alsaif@gmail.com>
2026-02-16 13:14:41 +0300

    Second commit, there is an error of urls

--- apps/bookmodule/views.py
+++ apps/bookmodule/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+# def index(request):
+#     name = request.GET.get("name") or "world!" #add this line
+#     return HttpResponse("Hello, "+name) #replace the word “world!” with the variable name
+
+
+def index2(request, val1 = 0): #add the view function (index2)
+     return HttpResponse("value1 = "+str(val1))
+
+def index(request):
+    name = request.GET.get("name") or "world!"
+    return render(request, "bookmodule/index.html" , {"name": name}) #your render line
