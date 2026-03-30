9a2f2cc637c40c8dae42e635f45dfb032f79688b
Rqyq <mohammed.sa.alsaif@gmail.com>
2026-03-30 12:27:00 +0300

    Lab 4 Done

--- website/urls.py
+++ website/urls.py
@@ -20,5 +20,5 @@ import apps.bookmodule.views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('books/', include("apps.bookmodule.urls")), #include urls.py of bookmodule app
-    path('users/', include("apps.usermodule.urls")) #include urls.py of usermodule app
+    path('users/', include("apps.usermodule.urls")), #include urls.py of usermodule app
 ]
