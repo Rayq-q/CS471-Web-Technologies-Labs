e0bc4f51649d29d368d282d768681b3e014f8472
Rqyq <mohammed.sa.alsaif@gmail.com>
2026-04-13 13:27:23 +0300

    Lab 5 DONE

--- apps/bookmodule/urls.py
+++ apps/bookmodule/urls.py
@@ -6,5 +6,9 @@ path('', views.index, name= "books.index"),
 path('list_books/', views.list_books, name= "books.list_books"),
 path('<int:bookId>/', views.viewbook, name="books.view_one_book"),
 path('aboutus/', views.aboutus, name="books.aboutus"),
+
+path('html5/text/formatting/', views.formatting, name="books.formatting"),
+path('html5/listing/', views.listing, name="books.listing"),
+path('html5/tables/', views.tables, name="books.tables"),
 path('html5/links/', views.links, name="books.links"),
 ]
